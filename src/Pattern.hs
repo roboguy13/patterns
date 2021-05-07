c3169e34992b68032e38368ae12936e85c6c3f25
David Young <roboguy06@gmail.com>
2021-05-06 21:56:27 -0500

    Fixed significant typo in InRPat; changed the types a bit, so now order of matches shouldn't matter

--- src/Pattern.hs
+++ src/Pattern.hs
@@ -4,6 +4,7 @@
 {-# LANGUAGE TypeFamilies #-}
 {-# LANGUAGE PatternSynonyms #-}
 {-# LANGUAGE AllowAmbiguousTypes #-}
+{-# LANGUAGE QuantifiedConstraints #-}
 
 import           Data.Functor.Foldable
 import           Data.Kind
@@ -21,8 +22,9 @@ data ADT t where
   Add :: ADT Int -> ADT Int -> ADT Int
 
   -- | Similar to Mark Tullsen's First Class Patterns paper
-  (:->) :: Pattern ADT s t -> (t -> ADT r) -> ADT (PatFn t r)
-  (:|) :: ADT (PatFn a r) -> ADT (PatFn b r) -> ADT (PatFn (Either a b) r)
+  (:->) :: Pattern ADT (ADT s) t -> (t -> ADT r) -> ADT (PatFn s r)
+  (:|) :: ADT (PatFn (Either a b) r) -> ADT (PatFn (Either a b) r) -> ADT (PatFn (Either a b) r)
+  -- (:|) :: ADT (PatFn a r) -> ADT (PatFn b r) -> ADT (PatFn (Either a b) r)
 
   Apply :: (a --> b) -> ADT a -> ADT b
 
@@ -31,7 +33,7 @@ data ADT t where
 
   -- Rec :: ADT (RecLayer ADT [a]) -> ADT [a]
 
-  TailRec :: ((a --> b) -> (a --> b)) -> ADT (PatFn a b)
+  Rec :: ((a --> b) -> (a --> b)) -> ADT (PatFn a b)
 
 type x --> y = ADT (PatFn (ERepTy x) y)
 
@@ -39,33 +41,46 @@ data Pattern f s t where
   BasePat :: Pattern f (f a) a
   PairPat :: Pattern f (f (a, b)) (f a, f b)
   InLPat :: Pattern f (f (Either a b)) (f a)
-  InRPat :: Pattern f (f (Either a b)) (f a)
+  InRPat :: Pattern f (f (Either a b)) (f b)
   CompPat :: Pattern f a b -> Pattern f b c -> Pattern f a c
   -- MatchRec :: ... ?
 
-(.->) :: Pattern ADT s t -> (t -> ADT r) -> ADT (PatFn t r)
+(.->) :: Pattern ADT (ADT s) t -> (t -> ADT r) -> ADT (PatFn s r)
 (.->) = (:->)
 
-(.|) :: ADT (PatFn a r) -> ADT (PatFn b r) -> ADT (PatFn (Either a b) r)
+-- (.|) :: ADT (PatFn a r) -> ADT (PatFn b r) -> ADT (PatFn (Either a b) r)
+(.|) :: ADT (PatFn (Either a b) r) -> ADT (PatFn (Either a b) r) -> ADT (PatFn (Either a b) r)
 (.|) = (:|)
 
 type family ERepTy t
-type instance ERepTy [a] = Either () (ADT a, ADT [a])
+type instance ERepTy [a] = Either () (a, ADT [a])
 type instance ERepTy (a, b) = (a, b)
 type instance ERepTy (Either a b) = Either a b
 type instance ERepTy () = ()
 
+--NilPat :: Pattern f (f (Either t b2))
+-- pattern NilPat :: Pattern ADT (ADT (Either () (a, [a]))) ()
 pattern NilPat  = CompPat InLPat BasePat
+
+--Pattern f (f (Either (a2, b3) b2)) (f a2, f b3)
+--Pattern f (f (Either a1 (a2, b3))) (f a2, f b3)
+-- pattern ConsPat :: Pattern ADT (ADT (Either () (a, [a]))) (ADT a, ADT [a])
 pattern ConsPat = CompPat InRPat PairPat
 
 adtSum :: [Int] --> Int
-adtSum = TailRec $ \rec ->
+adtSum = Rec $ \rec ->
   (NilPat  .-> \()      -> Value 0) .|
-  (ConsPat .-> \(x, xs) -> Add x (Apply rec xs))
+  (ConsPat .-> \(x, xs) -> Add x (Apply undefined xs))
 
-runMatch :: ([a] --> b) -> ADT [a] -> ADT b
-runMatch (CompPat InLPat BasePat :-> f) Nil' = f (Left ())
-runMatch (CompPat InRPat PairPat :-> f) (Cons' x xs) = f (Right (x, xs))
+  -- (ConsPat .-> \(x, xs) -> Add x (Apply rec xs)) .|
+  -- (NilPat  .-> \()      -> Value 0)
+
+-- runMatch :: ([a] --> b) -> ADT [a] -> ADT b
+runMatch :: ERepTy [a] ~ Either () (ADT a, ADT [a]) => ADT (PatFn (ERepTy [a]) b) -> ADT [a] -> ADT b
+runMatch = undefined
+-- runMatch (CompPat InLPat BasePat :-> f) Nil' = f (Left ())
+-- runMatch (ConsPat :-> f) (Cons' x xs) = _ --f (Right (x, xs))
+-- runMatch (ConsPat :-> f) (Cons' x xs) = f (x, xs)
 
 -- runMatch (NilPat  :-> f) Nil' = f (Left ())
 -- runMatch (ConsPat :-> f) (Cons' x xs) = f (Right (x, xs))
